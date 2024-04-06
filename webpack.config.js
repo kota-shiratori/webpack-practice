e7c852b55d5c05cc15aa02bcd9735ac2d46f1fa5
kota_shiratori <swan.kouta@gmail.com>
2024-04-06 23:31:08 +0900

    ⚡️ Sassファイル読み込み

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,23 @@
+const path = require('path');
+
 module.exports = {
     mode: 'development',
-    devtool: 'none',
+    devtool: 'eval-source-map',
     entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname,'public'),
+        filename: 'app.bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ] 
+            }
+        ]
+    }
 }
