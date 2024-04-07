ae97c3691c5c7ca8b33b86c83f5fb510455293b6
kota_shiratori <swan.kouta@gmail.com>
2024-04-07 21:58:41 +0900

    ⚡️ ファイルローダー

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,27 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    mode: 'development',
-    devtool: 'eval-source-map',
-    entry: './src/app.js',
-    output: {
-        path: path.resolve(__dirname,'public'),
-        filename: 'app.bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'postcss-loader',
-                    'sass-loader'
-                ] 
-            }
-        ]
-    }
-}
+  mode: "development",
+  devtool: "eval-source-map",
+  entry: "./src/app.js",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "app.bundle.js",
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif|webp|svg)$/,
+        type: "asset/resource",
+        generator: {
+          filename: "images/[name][ext]",
+        },
+      },
+    ],
+  },
+};
