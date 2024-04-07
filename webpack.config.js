82a3e512048c7adebfc3c7ab3ab8b696c549c738
kota_shiratori <swan.kouta@gmail.com>
2024-04-07 21:27:19 +0900

    ⚡️ autoprefixer

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,7 @@ module.exports = {
                 use: [
                     'style-loader',
                     'css-loader',
+                    'postcss-loader',
                     'sass-loader'
                 ] 
             }
